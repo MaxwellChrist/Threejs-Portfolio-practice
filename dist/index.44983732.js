b05e247078d732e2984bca75e36eec1e203be1dc
MaxwellChrist <ribthulu@gmail.com>
2022-06-24 13:31:33 -0400

    added basic css

---
+++ dist/index.44983732.js
@@ -0,0 +1,4 @@
+const scene = new THREE.scene();
+console.log(scene);
+
+//# sourceMappingURL=index.44983732.js.map
--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log(THREE)
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.append(renderer.domElement)
